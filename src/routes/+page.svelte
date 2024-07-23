be34928e42f319a2bbc3e21fd9a4c093f9c7880a
Luís Miranda <lantoniomiranda@ipvc.pt>
2024-07-23 17:19:00 +0100

    added some seeders to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,64 @@
-<script>
-    import { page } from '$app/stores';
-    import user from '$lib/images/user.png';
-    import HolidayCountDown from '$lib/components/HolidayCountDown.svelte';
+<script lang="ts">
+import { page } from '$app/stores';
+import HolidayCountDown from '$lib/components/HolidayCountDown.svelte';
+	import Quotes from '$lib/components/Quotes.svelte';
+import type { Holiday } from '$lib/types/holiday';
+
+export let holidays: Holiday[] = [
+    {
+        country: 'Portugal',
+        iso: 'PT',
+        year: 2024,
+        date: '2024-01-01',
+        day: 'Monday',
+        name: 'New Year\'s Day',
+        type: 'national_holiday',
+    },
+    {
+        country: 'Portugal',
+        iso: 'PT',
+        year: 2024,
+        date: '2024-04-06',
+        day: 'Saturday',
+        name: 'Easter',
+        type: 'religious_holiday',
+    },
+    {
+        country: 'Portugal',
+        iso: 'PT',
+        year: 2024,
+        date: '2024-06-10',
+        day: 'Monday',
+        name: 'Portugal Day',
+        type: 'national_holiday',
+    },
+];
+
+export let quotes: string[] = [
+    "A Bro is always entitled to do something stupid, as long as the rest of his Bros are all doing it.",
+    "I'm a guy who has faked love his entire life",
+    "Whatever you do in this life, it's not legendary, unless your friends are there to see it.",
+    "When I get sad, I stop being sad and be awesome instead.",
+    "A lie is just a great story that someone ruined with the truth.",
+    "New is always better.",
+    "Think of me like Yoda, but instead of being little and green, I wear suits and I'm awesome. I'm your bro—I'm Broda!",
+    "Whatever you do in this life, it's not legendary unless your friends are there to see it.",
+    "If you have a crazy story, I was there. It’s just a law of the universe.",
+    "Barney Stinson: That guy's awesome!"
+];
+
+
 </script>
 
-<svelte:head>
-    
-</svelte:head>
-
-<section class="flex flex-col">
-    <div class="flex justify-between items-center">
-        <img src={user} alt="User icon" class="h-32 w-32 rounded-full" />
-        <div class="text-content">
-            <p class="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <p class="text-lg">Nisi quas nesciunt voluptatum, vitae dicta, fuga non deserunt impedit perspiciatis, quae dolorum accusamus quos odit.</p>
-            <p class="text-lg">Tempora sit ut a saepe provident!</p>
-        </div>
+<section class="flex flex-col items-center justify-center h-screen">
+    <div class="w-full max-w-screen-xl">  
+        <Quotes {quotes} />
     </div>
-    <div class="flex justify-center">
-        <div class="countdown-container flex flex-wrap gap-4">
-            <div class="countdown-item p-4 rounded-md">
-                <HolidayCountDown />
-            </div>
+    <div class="countdown-container flex flex-wrap gap-4">
+        <div class="countdown-item p-4 rounded-md">
+            <HolidayCountDown {holidays} />
         </div>
     </div>
 </section>
+
+
