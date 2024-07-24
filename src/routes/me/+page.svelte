2af891b2b17bf357fe68b58d7b77f26329a5b098
Luís Miranda <lantoniomiranda@ipvc.pt>
2024-07-24 13:25:50 +0100

    removed unused routes

---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import user from '$lib/images/user.png';
+</script>
+
+<section class="flex flex-col md:flex-row items-center justify-between h-screen">
+	<div class="w-full md:w-1/2">
+        <img alt="Luís Miranda" src={user} class="rounded-full shadow-lg w-auto h-auto max-w-md mx-auto md:mx-0" />
+	</div>
+	<div class="w-auto h-auto md:w-1/2 px-8">
+		<h1 class="text-4xl font-bold mb-4">me, myself and I</h1>
+		<p class="text-md mb-6">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p class="text-md mb-6">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p class="text-md">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+</section>
