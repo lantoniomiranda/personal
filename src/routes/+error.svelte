670592a5d39abb2c30e8a441a4d92bccdb555f7a
Luís Miranda <lantoniomiranda@ipvc.pt>
2024-07-24 11:56:45 +0100

    added error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+  </script>
+  
+  <div class="flex flex-col items-center justify-center h-screen  text-white">
+    <h1 class="text-4xl font-bold mb-4">Oops!</h1>
+    <p class="text-lg mb-8">That page couldn't be found.</p>
+    <a href="/" class="hover:text-orange-500 text-white font-bold py-2 px-4">go yo</a>
+  </div>
+  
