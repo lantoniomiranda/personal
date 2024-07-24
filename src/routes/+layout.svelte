19d0a70652c91f8f70f5dc2e5c27884d50118991
Luís Miranda <lantoniomiranda@ipvc.pt>
2024-07-24 11:36:57 +0100

    better layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,24 @@
 <script>
-    import Header from '../lib/components/Header.svelte';
-    import '../app.css';
+	import Header from '../lib/components/Header.svelte';
+	import '../app.css';
 </script>
 
-<div class="app bg-yellow-950 bg-opacity-75 min-h-screen text-white">
-    <Header />
+<div class="flex flex-col h-screen bg-yellow-950 bg-opacity-75 text-white overflow-hidden">
+	<header class="fixed top-0 left-0 right-0 z-10 px-4 py-2">
+		<Header />
+	</header>
 
-    <main class="flex flex-col flex-grow p-4 w-full max-w-md mx-auto">
-        <slot />
-    </main>
+	<main class="flex-grow">
+		<div class="container mx-auto py-8">
+			<slot />
+		</div>
+	</main>
 
-    <footer class="flex flex-col justify-center items-center p-4">
-        <p>developed with <a class="text-red-400" href="https://kit.svelte.dev" target="_blank">SvelteKit</a></p>
-    </footer>
+	<footer class="flex-shrink-0 p-4">
+		<p class="text-center">
+			developed with <a href="https://kit.svelte.dev" target="_blank" class="text-red-400"
+				>SvelteKit</a
+			>
+		</p>
+	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,50 @@
 <script lang="ts">
-import { page } from '$app/stores';
-import HolidayCountDown from '$lib/components/HolidayCountDown.svelte';
+	import { page } from '$app/stores';
+	import HolidayCountDown from '$lib/components/HolidayCountDown.svelte';
 	import Quotes from '$lib/components/Quotes.svelte';
-import type { Holiday } from '$lib/types/holiday';
+	import type { PageData } from './$types';
 
-export let holidays: Holiday[] = [
-    {
-        country: 'Portugal',
-        iso: 'PT',
-        year: 2024,
-        date: '2024-10-01',
-        day: 'Monday',
-        name: 'New Year\'s Day',
-        type: 'national_holiday',
-    },
-    {
-        country: 'Portugal',
-        iso: 'PT',
-        year: 2024,
-        date: '2024-09-06',
-        day: 'Saturday',
-        name: 'Easter',
-        type: 'religious_holiday',
-    },
-    {
-        country: 'Portugal',
-        iso: 'PT',
-        year: 2024,
-        date: '2024-12-10',
-        day: 'Monday',
-        name: 'Portugal Day',
-        type: 'national_holiday',
-    },
-];
+	export let data: PageData;
+	let holidays: any = data.data;
 
-export let quotes: string[] = [
-    "A Bro is always entitled to do something stupid, as long as the rest of his Bros are all doing it.",
-    "I'm a guy who has faked love his entire life",
-    "Whatever you do in this life, it's not legendary, unless your friends are there to see it.",
-    "When I get sad, I stop being sad and be awesome instead.",
-    "A lie is just a great story that someone ruined with the truth.",
-    "New is always better.",
-    "Think of me like Yoda, but instead of being little and green, I wear suits and I'm awesome. I'm your bro—I'm Broda!",
-    "Whatever you do in this life, it's not legendary unless your friends are there to see it.",
-    "If you have a crazy story, I was there. It’s just a law of the universe.",
-    "Barney Stinson: That guy's awesome!"
-];
-
-
+	export let quotes: string[] = [
+		'A Bro is always entitled to do something stupid, as long as the rest of his Bros are all doing it.',
+		"I'm a guy who has faked love his entire life",
+		"Whatever you do in this life, it's not legendary, unless your friends are there to see it.",
+		'When I get sad, I stop being sad and be awesome instead.',
+		'A lie is just a great story that someone ruined with the truth.',
+		'New is always better.',
+		"Think of me like Yoda, but instead of being little and green, I wear suits and I'm awesome. I'm your bro—I'm Broda!",
+		"Whatever you do in this life, it's not legendary unless your friends are there to see it.",
+		'If you have a crazy story, I was there. It’s just a law of the universe.',
+		"Barney Stinson: That guy's awesome!"
+	];
 </script>
 
-<section class="flex flex-col items-center justify-center h-screen">
-    <div class="w-full max-w-screen-xl">  
-        <Quotes {quotes} />
-    </div>
-    <div class="countdown-container flex flex-wrap gap-4">
-        <div class="countdown-item p-4 rounded-md">
-            <HolidayCountDown {holidays} />
-        </div>
-    </div>
+<section class="flex flex-col items-center justify-center min-h-screen">
+	<div class="w-full max-w-screen-xl animate-custom">
+		<Quotes {quotes} />
+	</div>
+	<div class="countdown-container flex flex-wrap gap-4">
+		<div class="countdown-item p-4 rounded-md">
+			<HolidayCountDown {holidays} />
+		</div>
+	</div>
 </section>
 
-
+<style>
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+  
+    .animate-custom {
+      animation: fadeIn 1s ease-in-out;
+    }
+  </style>
